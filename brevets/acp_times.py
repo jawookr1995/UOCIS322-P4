e203799e7bcaf0f68e5b4be9f67d40ea2f291a5c
Theodore Yun <68358248+jawookr1995@users.noreply.github.com>
2021-05-14 18:39:07 -0700

    slight changes

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -24,44 +24,61 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        This will be in the same time zone as the brevet start time.
     """
 
-    # Check that the control is not more than 120% the brevet
-    if ((brevet_dist_km * 1.2) < control_dist_km):
-        return arrow.get(brevet_start_time).replace(days=- 182).isoformat()
-
-    # Ensure the corrct control dist is used in calculations over the brevet dist
-    if (brevet_dist_km <= control_dist_km):
+    if (brevet_dist_km < control_dist_km):
         control_dist_km = brevet_dist_km
 
-    done = False
-    dist_travel = 0
-    i = 0
-    time = 0
-    prev = 0
-    control = control_dist_km
+        # we are now going to get the time for the route with our corr.
+        # max values
+    rawTime = timeCalculator(control_dist_km, maxSpeedOnly)
 
-    while (not done):
-        # if True then control > brevet dist. Final calc
-        if (dist_travel + control) <= B_TABLE[i][0]:
-            time += control / B_TABLE[i][2]
-            done = True
-        # Calculate the segment's time
-        else:
-            prev = B_TABLE[i][0] - dist_travel
-            time += prev / B_TABLE[i][2]
-            i += 1
-            control -= prev
-            dist_travel += prev
+    # once we know how much time the route took, simply add it to the
+    # initial time and we are good to go
+    finalTime = timeAdder(rawTime, brevet_start_time)
+    return finalTime
 
-    min = time % 1
-    hr = time - min
-    min = round(min * 60)
+def timeAdder(rawTime, initialTime):
 
-    bst = arrow.get(brevet_start_time)
-    bst = bst.replace(hours=+ hr)
-    bst = bst.replace(minutes=+ min)
+    #multiply by 60 to get into minutes. Then adding .5 will ensure that
+    #any left over gets rounded, as changing it to an int with the command
+    #below will take care of any decimal still there
+    #**testing appeared to make the .5 necessary to round up and get values
+    #more in line with the actual calculator
+    totMinutes = ((rawTime * 60) + .5)
+    int(totMinutes)
 
-    return bst.isoformat()
+    #use our arrow functions to add the minutes from timeCalculator() and
+    #translate that into a formatted time
+    finalTime = arrow.get(initialTime).shift(minutes=+totMinutes).isoformat()
 
+    return finalTime
+
+def timeCalculator(kmGiven, speedTable):
+    #keeps on chopping off 200 and going down the speeds
+    #initializers
+    x = 0
+    totTime = 0
+
+    #There are intervals of 200. So essentially what this function
+    #does is cycle through these intervals. So long as its greater than
+    #200 (which signifies a new interval) we will keep cycling through the
+    #main part of the min/max speed values. We also have x < 3 b/c we don't
+    #want it to somehow run more than 4 times inside the loop (as the last
+    #line before the return statement is the 5th, if any, pass through). This
+    #is b/c our min/max only have 5 speed intervals each.
+    #It will keep going deeper into min/maxSpeedOnly until it runs out from
+    #being chopped off by 200 for each interval pass.
+    while kmGiven > 200:
+        if x < 3:
+            totTime = totTime + (200/speedTable[x])
+            x += 1
+            kmGiven = kmGiven - 200
+
+    #4th pass right here or 1st if less than 200
+    #this will be 200 or the remainder (something less than 200) for
+    #the final interval pass to evaluate
+    totTime = totTime + (kmGiven/speedTable[x])
+
+    return totTime
 
 
 
@@ -78,50 +95,14 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    # Check that the control is not more than 120% the brevet
-    if ((brevet_dist_km * 1.2) < control_dist_km):
-        return arrow.get(brevet_start_time).replace(days=- 182).isoformat()
+    if control_dist_km == 0:
+        finalTime = timeAdder(1, brevet_start_time)
+        return finalTime
 
-    # Ensure that closing controls after the brevet dist
-    # all have the same closing time
-    if (control_dist_km >= brevet_dist_km):
-        min = MAX_TIME[brevet_dist_km][1]
-        hr = MAX_TIME[brevet_dist_km][0]
+    else:
 
-        bst = arrow.get(brevet_start_time)
-        bst = bst.replace(hours=+ hr)
-        bst = bst.replace(minutes=+ min)
-
-        return bst.isoformat()
-
-    done = False
-
-    dist_travel = 0
-    i = 0
-    time = 0
-    prev = 0
-    control = control_dist_km
-
-    while (not done):
-        # if True then control > brevet dist. Final calc
-        if (dist_travel + control) <= B_TABLE[i][0]:
-            time += control / B_TABLE[i][1]
-            done = True
-        # Calculate the segment's time
-        else:
-            prev = B_TABLE[i][0] - dist_travel
-            time += prev / B_TABLE[i][1]
-            i += 1
-            control -= prev
-            dist_travel += prev
-
-    # print('time in hours:', time)
-    min = time % 1
-    hr = time - min
-    min = round(min * 60)
-
-    bst = arrow.get(brevet_start_time)
-    bst = bst.replace(hours=+ hr)
-    bst = bst.replace(minutes=+ min)
-
-    return bst.isoformat()
+        # very similar to open_time. Calculate the time w/ the intervals,
+        # then add it to the initial time and return
+        rawTime = timeCalculator(control_dist_km, minSpeedOnly)
+        finalTime = timeAdder((rawTime), brevet_start_time)
+        return finalTime
